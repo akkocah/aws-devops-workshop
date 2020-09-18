4e47aeec2846872ab3916dd0718ab1f6fe179776
akkocah <67174444+akkocah@users.noreply.github.com>
2020-09-18 03:09:45 +0300

    n

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -27,4 +27,5 @@ def result():
     return render_template("result.html", number_decimal = number, number_roman =convert(number) )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)

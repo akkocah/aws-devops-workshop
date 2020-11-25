2a541370c7172317ac8f0025f6e712fe84f6dde2
akkocah <67174444+akkocah@users.noreply.github.com>
2020-11-25 23:40:36 +0300

    I added project 202

---
+++ devops/projects/202-jenkins-pipeline-for-phonebook-app-on-docker-swarm/clasnotes.sh
@@ -0,0 +1,14 @@
+Project-202: Docker Swarm Deployment of Phonebook Application (Python Flask) with
+- Infrastructure
+    - Public Repository on Github (Codebase, Versionig System)
+    - Docker Swarm as Orchestrator
+        - 3 Manager
+        - 2 Worker
+    - Image Repository (AWS ECR)
+    - Should be able to
+        - Every EC2 is able to talk each other (EC2 Connect CLI, IAM Policy)
+        - Grand Master can pull image from ECR and push image to AWS ECR
+        - Mangers and Workers can pull image from AWS ECR.
+- Application Deployement
+    - Dockerfile
+    - docker-compose.yml (Web server and Mysql)
